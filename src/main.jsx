2b745acce4fe61339765bbb6b5ca694ef10b8686
KingTugce <kingstugce@gmail.com>
2023-06-20 19:36:46 -0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -11,14 +11,23 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
-/* existing imports */
-
 import Root from "./routes/root";
+import ErrorPage from "./error_page";
+import Contact from "./routes/contact";
+import Root,{ loader as rootLoader} from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
