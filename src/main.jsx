a752b428005a0bce830d96b3152f02de4ca753bb
KingTugce <kingstugce@gmail.com>
2023-06-22 16:33:38 -0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -16,8 +16,10 @@ import Contact, {
   loader as contactLoader,
 } from "./routes/contact";
 import Root,{ 
-  loader as rootLoader, action as rootAction,
+  loader as rootLoader, 
+  action as rootAction,
 } from "./routes/root";
+import EditContact from "./routes/edit";
 
 const router = createBrowserRouter([
   {
@@ -30,6 +32,11 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
         loader: contactLoader,
       },
     ],
