3d2d17cddb98e93a6ecc60a45d5f792155b1f8f0
KingTugce <kingstugce@gmail.com>
2023-06-27 16:16:44 -0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import "./index.css";
 import ErrorPage from "./error_page";
 import Contact, {
   loader as contactLoader,
+  action as contactAction,
 } from "./routes/contact";
 import Root,{ 
   loader as rootLoader, 
@@ -38,6 +39,7 @@ const router = createBrowserRouter([
         path: "contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
+        action: contactAction,
       },
       {
         path: "contacts/:contactId/edit",
--- src/routes/destroy.jsx
+++ src/routes/destroy.jsx
@@ -1,5 +1,7 @@
 import { redirect } from "react-router-dom";
 import { deleteContact } from "../contacts";
+import { action as destroyAction } from "./routes/destroy";
+
 
 export async function action({ params }) {
     throw new Error("oh dang!");
